cca76cea1cf5e8f6435bd8383b71da0900641eea
Andrew McKnight <git@mcknight.rocks>
2025-02-11 14:47:56 -0900

    add defaultValueDescription override to Renderer conformance of ExpressibleByArgument

--- Sources/xcbeautify/OutputFormat+ExpressibleByArgument.swift
+++ Sources/xcbeautify/OutputFormat+ExpressibleByArgument.swift
@@ -5,4 +5,17 @@ import ArgumentParser
 #endif
 import XcbeautifyLib
 
-extension XcbeautifyLib.Renderer: ExpressibleByArgument { }
+extension XcbeautifyLib.Renderer: ExpressibleByArgument {
+    var defaultValueDescription: String {
+        switch self {
+        case .terminal:
+            "Local"
+        case .gitHubActions:
+            "GitHub actions (`GITHUB_ACTIONS` is defined as `true`)"
+        case .teamcity:
+            "TeamCity (`TEAMCITY_VERSION` is not `nil`)"
+        case .azureDevOpsPipelines:
+            "Azure DevOps Pipelines (`AZURE_DEVOPS_PIPELINES` is not `nil`)"
+        }
+    }
+}
